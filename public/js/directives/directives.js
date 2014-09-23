423158b9f55129a81759310da078093ae00b5e3c
Mauricio Gamboa <magalocr@gmail.com>
2014-09-23 11:35:29 -0600

    Home Carousel

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -63,17 +63,17 @@
     };
   }])
 
-  .directive('fullHeight', ['$window', function() {
+  .directive('fullHeight', ['$window', function($window) {
     return {
       restrict: 'A',
       link: function(scope, element, attrs, controller) {
-        var height = window.innerHeight;
+        var height = $window.innerHeight;
         element.css({
           'height': height
         });
         
         $(window).resize(function() {
-          var newHeight = window.innerHeight;
+          var newHeight = $window.innerHeight;
           element.css({
             'height': newHeight
           });
@@ -135,6 +135,38 @@
             }
           }
         });
+      }
+    };
+  }])
+
+  .directive('homeCarousel', [function() {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var current = 1;
+        var footerBgContainer = $('.bgs');
+        
+        element.on('slide.bs.carousel', function (e) {
+          setFooterBackground((current % 5) + 1);
+          current++;
+        });
+
+        function setFooterBackground(bgNumber) {
+          var prev = footerBgContainer.find('.bg');
+
+          setTimeout(function () {
+            prev.remove();
+          }, 4100);
+
+          var el = document.createElement('div');
+          el.className += 'bg bg' + bgNumber;
+
+          footerBgContainer.append(el);
+
+          setTimeout(function () {
+            el.className += ' show';
+          }, 20);
+        }
       }
     };
   }])
