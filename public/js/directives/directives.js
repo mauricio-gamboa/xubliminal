89636b06c330ed0be6183992aa44e20fb6b5a3c7
Mauricio Gamboa <mgamboa@mobiquityinc.com>
2014-10-18 17:01:19 -0600

    Services Gallery

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -351,7 +351,7 @@
     };
   }])
 
-  .directive('owlCarouselRepeat', ['$timeout', function ($timeout) {
+  .directive('owlCarouselRepeat', [function () {
     return {
       restrict: 'A',
 
@@ -364,6 +364,24 @@
             singleItem: true,
             stopOnHover: true,
             lazyLoad: true
+          })
+        }
+      }
+    };
+  }])
+
+  .directive('owlCarouselServices', [function () {
+    return {
+      restrict: 'A',
+
+      link: function (scope, element, attrs) {
+        if (scope.$last === true) {
+          var carousel = element.parent();
+          
+          carousel.owlCarousel({
+            autoPlay: 5000,
+            items: 4,
+            stopOnHover: true
           })
         }
       }
