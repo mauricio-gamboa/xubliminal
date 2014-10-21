4705fb5a0aa3712ce80ae1d7f8151219c9728ef8
Mauricio Gamboa <magalocr@gmail.com>
2014-10-21 00:25:05 -0600

    Services to Pages

--- public/js/controllers/servicesCtrl.js
+++ public/js/controllers/servicesCtrl.js
@@ -3,11 +3,13 @@
 
   angular.module('xubliminalApp.controllers')
 
-  .controller('ServicesCtrl', ['$log', 'xubServices', function ($log, xubServices) {
+  .controller('ServicesCtrl', ['$log', '$routeParams', 'xubServices', function ($log, $routeParams, xubServices) {
 
     this.servicesData = angular.bind(this, function (services) {
       if (!services.length) return;
       this.services = services;
+      this.tab = $routeParams.tab ? $routeParams.tab : 0;
+      this.service = this.services[this.tab];
     });
 
     this.errorServices = function (error) {
