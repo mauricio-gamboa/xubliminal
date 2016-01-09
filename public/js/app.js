195242ea44db7236ebd828df0667f0a084c76d2d
Mauricio Gamboa <magalocr@gmail.com>
2016-01-09 16:28:04 -0600

    New Porfolio

--- public/js/app.js
+++ public/js/app.js
@@ -1,51 +1,55 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular.module('xubliminalApp', ['ngRoute', 'ngAnimate', 'duScroll', 'angular-google-analytics', 'xubliminalApp.controllers', 'xubliminalApp.directives', 'xubliminalApp.services'])
+	angular.module('xubliminalApp', ['ngRoute', 'ngAnimate', 'duScroll', 'angular-google-analytics', 'xubliminalApp.controllers', 'xubliminalApp.directives', 'xubliminalApp.services'])
 
-  .config(function ($routeProvider, AnalyticsProvider) {
+		.config(function ($routeProvider, AnalyticsProvider) {
 
-    $routeProvider
+			$routeProvider
 
-    .when('/', {
-      templateUrl: 'partials/home.html'
-    })
+				.when('/', {
+					templateUrl: 'partials/home.html'
+				})
 
-    .when('/services', {
-      templateUrl: 'partials/services.html'
-    })
+				.when('/services', {
+					templateUrl: 'partials/services.html'
+				})
 
-    .when('/services/:service', {
-      templateUrl: 'partials/services.html'
-    })
+				.when('/services/:service', {
+					templateUrl: 'partials/services.html'
+				})
 
-    .when('/work', {
-      templateUrl: 'partials/work.html'
-    })
+				.when('/work', {
+					templateUrl: 'partials/work.html'
+				})
 
-    .when('/work-detail', {
-      templateUrl: 'partials/work-detail.html'
-    })
+				.when('/work-new', {
+					templateUrl: 'partials/work-new.html'
+				})
 
-    .when('/freebies', {
-      templateUrl: 'partials/freebies.html'
-    })
+				.when('/work-detail', {
+					templateUrl: 'partials/work-detail.html'
+				})
 
-    .when('/get-quote', {
-      templateUrl: 'partials/get-quote.html'
-    })
+				.when('/freebies', {
+					templateUrl: 'partials/freebies.html'
+				})
 
-    .otherwise({
-      redirectTo: '/',
-    });
+				.when('/get-quote', {
+					templateUrl: 'partials/get-quote.html'
+				})
 
-    AnalyticsProvider.setAccount('UA-41378737-1');
-    AnalyticsProvider.setDomainName('xubliminal.com');
-    AnalyticsProvider.ignoreFirstPageLoad(true);
-    AnalyticsProvider.setPageEvent('$routeChangeSuccess');
-  });
+				.otherwise({
+					redirectTo: '/',
+				});
 
-  angular.module('xubliminalApp.controllers', []);
-  angular.module('xubliminalApp.directives', []);
-  angular.module('xubliminalApp.services', []);
+			AnalyticsProvider.setAccount('UA-41378737-1');
+			AnalyticsProvider.setDomainName('xubliminal.com');
+			AnalyticsProvider.ignoreFirstPageLoad(true);
+			AnalyticsProvider.setPageEvent('$routeChangeSuccess');
+		});
+
+	angular.module('xubliminalApp.controllers', []);
+	angular.module('xubliminalApp.directives', []);
+	angular.module('xubliminalApp.services', []);
 }());
